df1543ca689b74264fcb9903a09239c7580f9dc9
cmodii <100476911+cmodii@users.noreply.github.com>
2023-06-04 13:59:56 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from threading import Thread
+from PIL import Image
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "ahoy"
+
+
+@app.route('/v1/get_image', methods=['GET'])
+def return_image():
+    return open("image.png", "rb").read()     
+
+
+def run():
+    app.run(host='localhost', port=7000)
+
+
+t = Thread(target=run)
+t.start()
